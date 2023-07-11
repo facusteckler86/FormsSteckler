a04399e1051477459ff313706a625983cd023c3b
facusteckler86 <facundomsteckler@gmail.com>
2023-07-11 01:37:01 -0300

    forth commit

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
+
 export class FormularioComponent {
+  LoginModel={
+    email: "",
+    password: "",
+    checkbox: "",
+
+
+  }
+email: any;
 
 }
